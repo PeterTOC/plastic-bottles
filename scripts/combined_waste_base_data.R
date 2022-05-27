fb5cc53fa573a6875f28d72b85cc040b97e0b0ce
Peter Boshe <peterboshe@gmail.com>
2022-05-27 09:41:06 +0300

    - combining script

---
+++ scripts/combined_waste_base_data.R
@@ -0,0 +1,32 @@
+# script to combine the public pollutants data
+
+# Author: Peter Boshe
+# Version: 2022-05-27
+
+# Packages
+library(tidyverse)
+library(janitor)
+library(glue)
+
+# Parameters
+ # input
+files_raw <- list.files(here::here("data-raw"), "*F.csv")
+paths <- map_chr(files_raw, ~ glue("data-raw/", .x))
+
+ # output
+
+file_out <- here::here("data/combined_waste_base_data.rds")
+
+# ============================================================================
+
+# Code
+
+# read all files
+all_files <- map(paths, read_csv)
+
+# save combined data
+all_files |>
+  bind_rows(.id = "batch") |>
+  write_rds(file_out)
+
+
